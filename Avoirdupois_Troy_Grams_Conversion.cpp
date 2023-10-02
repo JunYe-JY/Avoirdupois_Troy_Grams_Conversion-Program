7f3396b33c985105766be8ac5a9db8ad70a048f9
Jun Ye <142946246+JunYe-JY@users.noreply.github.com>
2023-10-02 15:04:40 -0400

    Add files via upload

---
+++ Avoirdupois_Troy_Grams_Conversion.cpp
@@ -0,0 +1,83 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+/*
+• 1 dram = 1.7718451953125 grams
+• 1 dram = 1/256 Avoirdupois pound2
+• 1 dram = 1/96 Troy (apothecaries) pound
+*/
+
+class Mass {
+private:
+  double massinDrams;
+
+public:
+  void setMassAvoirdupoisPounds();
+  void setMassTroyPounds();
+  void setMassMetricGrams();
+
+  void getMass();
+};
+
+void Mass::getMass() {
+  cout << endl << "The conversions are: " << endl;
+  cout << "Mass in Avoirdupois pounds is " << massinDrams / 256 << endl;
+  cout << "Mass in Troy pounds is " << massinDrams / 96 << endl;
+  cout << "Mass in grams is " << massinDrams * 1.7718451953125 << endl << endl;
+}
+
+void Mass::setMassAvoirdupoisPounds() {
+  double input;
+  cout << "Please enter a mass in Avoirdupois pounds: ";
+  cin >> input;
+  massinDrams = input * 256;
+}
+void Mass::setMassTroyPounds() {
+  double input;
+  cout << "Please enter a mass in Troy pounds: ";
+  cin >> input;
+  massinDrams = input * 96;
+}
+void Mass::setMassMetricGrams() {
+  double input;
+  cout << "Please enter a mass in grams: ";
+  cin >> input;
+  massinDrams = input / 1.7718451953125;
+}
+
+int main() {
+  int input;
+  Mass Mass;
+  cout << "Conversions: " << endl;
+
+  do {
+    cout << "Please enter 1 for Avoirdupois pounds, 2 for Troy "
+            "pounds, 3 for grams, or 0 to exit: ";
+    cin >> input;
+
+    switch (input) {
+    case 1: {
+      Mass.setMassAvoirdupoisPounds();
+      Mass.getMass();
+      break;
+    }
+    case 2: {
+      Mass.setMassTroyPounds();
+      Mass.getMass();
+      break;
+    }
+    case 3: {
+      Mass.setMassMetricGrams();
+      Mass.getMass();
+      break;
+    }
+    default: {
+      cout << "Please enter 1, 2, 3, or 0." << endl;
+    }
+    }
+  } while (input != 0);
+
+  cout << "Thanks for using the mass conversion program!";
+  return 0;
+}
